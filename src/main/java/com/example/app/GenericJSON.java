a1082c00e31de0bce3c2b2a86a453facf5f089b6
Alexander Vaneyev <vaneyev@mail.ru>
2019-12-09 17:54:22 +0300

    Add files via upload

---
+++ src/main/java/com/example/app/GenericJSON.java
@@ -0,0 +1,23 @@
+package com.example.app;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+
+public class GenericJSON {
+
+	private Map<String, Object> properties = new HashMap<String, Object>();
+	
+	@JsonAnyGetter
+	public Map<String, Object> getProperties() {
+		return properties;
+	}
+
+	@JsonAnySetter
+	public void add(String key, Object value) {
+		properties.put(key, value);
+	}
+
+}
