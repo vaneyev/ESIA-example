c96ed36046d75adb3c879de9c1ee1f32e5f0f089
Alexander Vaneyev <vaneyev@mail.ru>
2019-12-11 10:52:32 +0300

    Вывод дополнительной информации об идентификационном токене

--- src/main/java/com/example/app/Application.java
+++ src/main/java/com/example/app/Application.java
@@ -81,7 +81,7 @@ public class Application {
 			params.put("timestamp", timeStamp);
 			params.put("client_secret", sign(scope + timeStamp + clientId + state));
 
-			return "<a href=\"" + authurl + getParamsString(params) + "\">Login</a>";
+			return "<h1>ЕСИА</h1><a href=\"" + authurl + getParamsString(params) + "\">Вход</a>";
 
 		} catch (Exception ex) {
 			ex.printStackTrace();
@@ -124,6 +124,8 @@ public class Application {
 
 			String[] tokenArray = authResponse.getProperties().get("access_token").toString().split("[.]");
 			GenericJSON payload = mapper.readValue(Base64.getUrlDecoder().decode(tokenArray[1]), GenericJSON.class);
+			String[] id_tokenArray = authResponse.getProperties().get("id_token").toString().split("[.]");
+			String id_payload = new String(Base64.getUrlDecoder().decode(id_tokenArray[1]));
 
 			objURL = new URL(resturl + payload.getProperties().get("urn:esia:sbj_id"));
 			con = (HttpURLConnection) objURL.openConnection();
@@ -136,15 +138,18 @@ public class Application {
 			}
 
 			StringBuilder sb = new StringBuilder();
+			sb.append("<h1>Информация о пользователе</h1><br> Идентификатор: ");
 			sb.append(payload.getProperties().get("urn:esia:sbj_id"));
-			sb.append("<br>");
+			sb.append("<br> ФИО: ");
 			sb.append(esiaPerson.getProperties().get("firstName"));
 			sb.append(" ");
 			sb.append(esiaPerson.getProperties().get("middleName"));
 			sb.append(" ");
 			sb.append(esiaPerson.getProperties().get("lastName"));
-			sb.append("<br> Verify: ");
-			sb.append(verify(tokenArray[0] + "." + tokenArray[1], tokenArray[2]));
+			sb.append("<br> Подпись ЕСИА: ");
+			sb.append(verify(tokenArray[0] + "." + tokenArray[1], tokenArray[2])?"подтверждена":"не подтверждена");
+			sb.append("<br>Идентификационный токен:<br>");
+			sb.append(id_payload);
 
 			return sb.toString();
 
